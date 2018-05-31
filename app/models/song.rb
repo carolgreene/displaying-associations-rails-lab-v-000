75b4133e62f8c8b578c21e8c6f907c33350276fc
Carol Greene <carolgreene3@gmail.com>
2018-05-31 00:06:50 +0000

    automatically backed up by learn

--- app/models/song.rb
+++ app/models/song.rb
@@ -1,7 +1,11 @@
 class Song < ActiveRecord::Base
   belongs_to :artist
 
-  def artist_name 
+  def artist_name
     artist.name
+  end
+
+  def artist_title
+    artist_name + "-" + title
   end
 end
